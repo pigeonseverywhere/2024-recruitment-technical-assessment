0f8c044db7f469cf063f567818eedfc234597ba8
Yunshu <yunshudai2010@gmail.com>
2024-02-27 18:30:26 +1100

    rust component redone

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -1,22 +1,35 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
+use serde_json::{Value};
 
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
+    let mut string_len = 0;
+    let mut int_sum = 0;
 
+    for val in request.data {
+        if val.is_string() {
+            string_len += val.as_str().unwrap().len();
+        } else if val.is_number() {
+            int_sum += val.as_u64();
+        }
+
+    }
     let response = DataResponse {
-        
+        string_len,
+        int_sum
     };
 
     (StatusCode::OK, Json(response))
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct DataRequest {
-    // Add any fields here
+    data: Vec<Value>
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    string_len: u64,
+    int_sum: u64
 }
